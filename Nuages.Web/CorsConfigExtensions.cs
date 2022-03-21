c11791105daab434dae467c018f08786964176b7
Martin Massé <martin@nuages.org>
2022-03-21 07:48:00 -0400

    CorsConfigExtensions

---
+++ Nuages.Web/CorsConfigExtensions.cs
@@ -0,0 +1,34 @@
+namespace Nuages.Web;
+
+public static class CorsConfigExtensions
+{
+    // ReSharper disable once MemberCanBePrivate.Global
+    public const string AllowSpecificOrigins = "AllowSpecificOrigins";
+    private const string AllowAnyOrigins = "AllowAnyOrigins";
+
+    // ReSharper disable once UnusedMember.Global
+    public static void AddCors(this IServiceCollection services, string[] domains)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(AllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins(domains)
+                        .AllowCredentials()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+
+
+            options.AddPolicy(AllowAnyOrigins,
+                builder =>
+                {
+                    builder.AllowCredentials()
+                        .SetIsOriginAllowed(_ => true)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+        });
+    }
+}
