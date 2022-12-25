e3c03b7931d5ae5f0cdc0d5de447e00329e451ab
Martin Massé <martin@nuages.org>
2022-12-25 07:13:18 -0500

    Add MapCustomEndpoints on WebApplication

--- Nuages.Web/EndpointConfigExtension.cs
+++ Nuages.Web/EndpointConfigExtension.cs
@@ -15,4 +15,17 @@ public static class EndpointConfigExtension
             provider.ProvideEnpoints(endpoints);
         }
     }
+    
+    [ExcludeFromCodeCoverage]
+    // ReSharper disable once UnusedMember.Global
+    public static void MapCustomEndpoints(this WebApplication application)
+    {
+        using var scope = application.Services.CreateScope();
+        
+        var endpointProviders = scope.ServiceProvider.GetServices<IEndpointsProvider>();
+        foreach (var provider in endpointProviders)
+        {
+            provider.ProvideEnpoints(application);
+        }
+    }
 }
